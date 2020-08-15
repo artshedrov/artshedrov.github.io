5d35b2a79132a14f079feb05c02a8b7ce2ddeef0
Art Schedrov <44718441+artshedrov@users.noreply.github.com>
2020-08-15 17:30:25 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,28 @@
+window.onresize = function() {
+  if (window.innerWidth <= 760) {
+    projectSlider.params.slidesPerGroup = 1;
+    projectSlider.params.slidesPerView = 1;
+  } else if (window.innerWidth > 760) {
+    projectSlider.params.slidesPerGroup = 2;
+    projectSlider.params.slidesPerView = 2;
+  }
+}
+
+let projectSlider = new Swiper('.project__slider', {
+  slidesPerGroup: 2,
+  slidesPerView: 2,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.project__slider__btn--next',
+    prevEl: '.project__slider__btn--prev'
+  }
+});
+
+let arendaSlider = new Swiper ('.arenda-slider__wrapper', {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.arenda-slider__btn--next',
+    prevEl: '.arenda-slider__btn--prev'
+  }
+});
